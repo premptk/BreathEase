658bc7b0ae8b9b1c982d8d516985364ff7cca480
Prem R Pattanayak <prem.p.in@rupeek.com>
2024-05-19 16:40:36 +0530

    Timer Page: added timer and buttons

--- app/src/main/java/com/example/breatheasegit/MainActivity.kt
+++ app/src/main/java/com/example/breatheasegit/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.breatheasegit
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var progressText: TextView
+    private var i = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_layout);
+        setContentView(R.layout.breathing_page)
+
+        progressBar = findViewById(R.id.progress_bar)
+        progressText = findViewById(R.id.progress_text)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                if (i <= 100) {
+                    progressText.text = "$i"
+                    progressBar.progress = i
+                    i++
+                    handler.postDelayed(this, 200)
+                } else {
+                    handler.removeCallbacks(this)
+                }
+            }
+        }, 200)
     }
 }
